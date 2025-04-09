6f7b9bc195c3f9a9271f2009baa96ea75f981046
Natalie Johnstone <nhj@illinois.edu>
2025-04-09 17:36:25 -0500

    first pass at modal

--- src/ilw-modal.js
+++ src/ilw-modal.js
@@ -6,7 +6,10 @@ class Modal extends LitElement {
 
     static get properties() {
         return {
-            theme: { type: String, attribute: true }
+            theme: { type: String, attribute: true },
+            open: { type: Boolean, reflect: true },
+            size: { type: String, attribute: true },
+            _hasGraphic: { state: true, type: Boolean }
         };
     }
 
@@ -17,13 +20,64 @@ class Modal extends LitElement {
     constructor() {
         super();
         this.theme = '';
+        this.open = false;
+        this.size = 'medium';
+        this._hasGraphic = false;
+    }
+
+    /**
+     * Tracks the number of graphic elements in the card, so we can
+     * hide the graphics container if there's no graphics.
+     *
+     * @private
+     */
+    _slotsChanged() {
+        const images = this.shadowRoot.querySelector("slot[name=image]");
+        if (images.assignedElements().length > 0) {
+            this._hasGraphic = true;
+            return;
+        }
+        this._hasGraphic = false;
+    }
+
+
+    connectedCallback() {
+        super.connectedCallback();
+        document.addEventListener('click', this.handleExternalTrigger);
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        document.removeEventListener('click', this.handleExternalTrigger);
+    }
+
+    handleExternalTrigger = (event) => {
+        const target = event.target.closest('[data-modal-target]');
+        if (target && target.getAttribute('data-modal-target') === this.id) {
+            this.open = true;
+        }
+    };
+
+    closeModal() {
+        this.open = false;
     }
 
     render() {
         return html`
-            <div>
-                <slot></slot>
-            </div>
+          <div class="backdrop" @click=${this.closeModal} aria-hidden=${!this.open}>
+    <div class="modal ${this.size}" role="dialog" aria-labelledby="modal-title" aria-modal="true" @click=${e => e.stopPropagation()}>
+    <div class="modal-header">
+          <div class="modal-image ${this._hasGraphic ? '' : 'hidden'}">
+          <slot name="image" @slotchange=${this._slotsChanged}></slot>
+    </div>
+        <h2 id="modal-title"><slot name="title"></slot></h2>
+        <button class="close-btn" @click=${this.closeModal} aria-label="Close modal">&times;</button>
+    </div>
+        <div class="modal-body">
+            <slot></slot>
+        </div>
+    </div>
+    </div>
         `;
     }
 }
